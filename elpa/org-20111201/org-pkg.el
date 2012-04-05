fe7ab420c00f671d114b70c14ecc1322d3226e3c
Rajat Vig <rajat.vig@gmail.com>
2012-04-06 01:31:51 +0530

    updated some elpa packages - need to get it better tracked

--- elpa/org-20111201/org-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org" "20111201" "Outline-based notes management and organizer" nil)
---
+++ elpa/org-20120405/org-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org" "20120405" "Outline-based notes management and organizer" nil)
