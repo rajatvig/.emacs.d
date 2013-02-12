5ec35e39e746719b5f5a441909fd8498cc404d03
Rajat Vig <rajat.vig@gmail.com>
2013-02-12 00:09:17 -0800

    update of a lot of stuff from elpa and submodules

---
+++ elpa/rbenv-0.0.2/rbenv-autoloads.el
@@ -0,0 +1,38 @@
+;;; rbenv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (rbenv-use rbenv-use-corresponding rbenv-use-global)
+;;;;;;  "rbenv" "rbenv.el" (20761 62706))
+;;; Generated autoloads from rbenv.el
+
+(autoload 'rbenv-use-global "rbenv" "\
+activate rbenv global ruby
+
+\(fn)" t nil)
+
+(autoload 'rbenv-use-corresponding "rbenv" "\
+search for .ruby-version and activate the corresponding ruby
+
+\(fn)" t nil)
+
+(autoload 'rbenv-use "rbenv" "\
+choose what ruby you want to activate
+
+\(fn RUBY-VERSION)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("rbenv-pkg.el") (20761 62706 86048))
+
+;;;***
+
+(provide 'rbenv-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rbenv-autoloads.el ends here
---
+++ elpa/rbenv-0.0.2/rbenv-pkg.el
@@ -0,0 +1,1 @@
+(define-package "rbenv" "0.0.2" "Emacs integration for rbenv" (quote nil))
