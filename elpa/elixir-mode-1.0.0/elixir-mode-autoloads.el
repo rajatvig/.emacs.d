15fe4e0ba446fb7f9649792d9b32f8a0ccc97377
Rajat Vig <rajat.vig@gmail.com>
2013-10-10 22:27:11 -0700

    new elpa modules

---
+++ elpa/elixir-mode-1.0.0/elixir-mode-autoloads.el
@@ -0,0 +1,28 @@
+;;; elixir-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "elixir-mode" "elixir-mode.el" (21079 35442
+;;;;;;  0 0))
+;;; Generated autoloads from elixir-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.ex\\'" . elixir-mode))
+
+(add-to-list 'auto-mode-alist '("\\.exs\\'" . elixir-mode))
+
+;;;***
+
+;;;### (autoloads nil nil ("elixir-mode-pkg.el") (21079 35443 6023
+;;;;;;  0))
+
+;;;***
+
+(provide 'elixir-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; elixir-mode-autoloads.el ends here
