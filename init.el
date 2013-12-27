bdbaa289a5aab5ec2a2adf36dc6eb39c6b96842d
Sny <sny.aggarwal@gmail.com>
2013-12-28 01:27:54 +0530

    increasing the font size

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 85 :width normal :foundry "apple" :family "Aurulent Sans Mono")))))
+'(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "apple" :family "Aurulent Sans Mono")))))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 ;;(add-hook 'write-file-hooks (lambda () (untabify (point-min) (point-max))))
