f22d710a7097fdc047442951731a362fa53c9f8e
Rajat Vig <rajat.vig@gmail.com>
2014-01-10 16:08:00 -0800

    use consolas

--- init.el
+++ init.el
@@ -77,14 +77,14 @@
 
 (setq org-startup-indented t)
 
-(set-default-font "Droid Sans Mono-9")
+(set-default-font "Consolas-11")
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-'(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 95 :width normal :foundry "apple" :family "Droid Sans Mono")))))
+'(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Consolas")))))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 ;;(add-hook 'write-file-hooks (lambda () (untabify (point-min) (point-max))))
