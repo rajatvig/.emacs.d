b87cc12ce58665afc6dd04f4d21bca25f2aeaa84
Rajat Vig <rajat.vig@gmail.com>
2014-03-26 09:45:21 -0700

    fix the snippet locations

--- config/hooks.el
+++ config/hooks.el
@@ -22,7 +22,7 @@
 
 (autopair-global-mode)
 
-(setq yas/snippet-dirs "~/.emacs.d/elpa/yasnippet/snippets")
+(setq yas/snippet-dirs "~/.emacs.d/elpa/yasnippet-20140314.255/snippets")
 
 (yas/load-directory yas/snippet-dirs)
 
