82f21a376cab5faaa1e62d12dcd0db73da77a04d
Rajat Vig <rajat.vig@gmail.com>
2014-06-06 00:07:06 -0700

    elpa updates

---
+++ elpa/quack-20130126.1623/quack-autoloads.el
@@ -0,0 +1,18 @@
+;;; quack-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("quack-pkg.el" "quack.el") (21393 26703
+;;;;;;  130408 0))
+
+;;;***
+
+(provide 'quack-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; quack-autoloads.el ends here
