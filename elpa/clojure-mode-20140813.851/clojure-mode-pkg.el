acf9c93e471db24eb6783b49a632f95875a73800
Rajat Vig <rajat.vig@gmail.com>
2014-08-26 16:33:53 -0700

    elpa updates

--- elpa/clojure-mode-20140813.851/clojure-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clojure-mode" "20140813.851" "Major mode for Clojure code" (quote nil))
---
+++ elpa/elixir-mode-20140825.2236/elixir-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "elixir-mode" "20140825.2236" "Major mode for editing Elixir files" 'nil :url "https://github.com/elixir-lang/emacs-elixir" :keywords
+  '("languages" "elixir"))
