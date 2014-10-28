36c695df0e4e8f056187dc619b26d1047cd82518
Rajat Vig <rajat.vig@gmail.com>
2014-10-28 09:26:49 -0700

    changes for the init

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 
 (require 'package)
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 (add-to-list 'load-path "~/.emacs.d/config")
@@ -21,11 +20,16 @@
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 2)
  '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes (quote ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(custom-safe-themes
+	 (quote
+		("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(display-battery-mode t)
  '(display-time-mode t)
  '(edts-inhibit-package-check t)
  '(feature-use-rvm t)
+ '(haskell-mode-hook
+	 (quote
+		(turn-on-haskell-indent turn-on-haskell-indentation)))
  '(ido-everywhere t)
  '(ido-mode (quote both) nil (ido))
  '(indent-tabs-mode t)
@@ -37,7 +41,18 @@
  '(js2-indent-on-enter-key t)
  '(rspec-use-rvm t)
  '(ruby-comment-column 96)
- '(safe-local-variable-values (quote ((enconing . utf-8) (erlang-indent-level . 4) (encoding . utf-8) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby") (whitespace-line-column . 80) (lexical-binding . t))))
+ '(safe-local-variable-values
+	 (quote
+		((enconing . utf-8)
+		 (erlang-indent-level . 4)
+		 (encoding . utf-8)
+		 (ruby-compilation-executable . "ruby")
+		 (ruby-compilation-executable . "ruby1.8")
+		 (ruby-compilation-executable . "ruby1.9")
+		 (ruby-compilation-executable . "rbx")
+		 (ruby-compilation-executable . "jruby")
+		 (whitespace-line-column . 80)
+		 (lexical-binding . t))))
  '(setq tab-width t)
  '(setq-default c-basic-offset)
  '(show-paren-mode t)
@@ -45,8 +60,7 @@
  '(tab-stop-list (quote (2 4 6 8 10 12 14 16 18 20 22 24 26 28 30)))
  '(tab-width 2)
  '(tool-bar-mode nil)
- '(workgroups-mode t)
- '(edts-inhibit-package-check t))
+ '(workgroups-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
