314a4a864f9234f96b58771bd493fed964c15e39
Rajat Vig <rajat.vig@gmail.com>
2014-12-12 13:52:37 -0800

    elpa updates

--- init.el
+++ init.el
@@ -18,15 +18,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+	 [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+	 (vector "#4d4d4c" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#ffffff"))
  '(c-basic-offset 2)
  '(column-number-mode t)
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-eighties)))
  '(custom-safe-themes
 	 (quote
-		("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+		("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(display-battery-mode t)
  '(display-time-mode t)
  '(edts-inhibit-package-check t)
+ '(fci-rule-color "#d6d6d6")
  '(feature-use-rvm t)
  '(haskell-mode-hook
 	 (quote
@@ -62,6 +67,28 @@
  '(tab-stop-list (quote (2 4 6 8 10 12 14 16 18 20 22 24 26 28 30)))
  '(tab-width 2)
  '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#c82829")
+		 (40 . "#f5871f")
+		 (60 . "#eab700")
+		 (80 . "#718c00")
+		 (100 . "#3e999f")
+		 (120 . "#4271ae")
+		 (140 . "#8959a8")
+		 (160 . "#c82829")
+		 (180 . "#f5871f")
+		 (200 . "#eab700")
+		 (220 . "#718c00")
+		 (240 . "#3e999f")
+		 (260 . "#4271ae")
+		 (280 . "#8959a8")
+		 (300 . "#c82829")
+		 (320 . "#f5871f")
+		 (340 . "#eab700")
+		 (360 . "#718c00"))))
+ '(vc-annotate-very-old-color nil)
  '(workgroups-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -80,4 +107,3 @@
 (load "osx")
 (load "hooks")
 (load "rvm-help")
-(load "theme")
