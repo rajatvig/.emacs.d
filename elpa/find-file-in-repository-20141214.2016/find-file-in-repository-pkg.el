f50bff7a28bdb52af854243b23a6b668ee9f7f97
Rajat Vig <rajat.vig@gmail.com>
2014-12-17 16:42:28 -0800

    ELPA:

---
+++ elpa/find-file-in-repository-20141214.2016/find-file-in-repository-pkg.el
@@ -0,0 +1,1 @@
+(define-package "find-file-in-repository" "20141214.2016" "Quickly find files in a git, mercurial or other repository" 'nil :url "https://github.com/hoffstaetter/find-file-in-repository" :keywords '("files" "convenience" "repository" "project" "source control"))
