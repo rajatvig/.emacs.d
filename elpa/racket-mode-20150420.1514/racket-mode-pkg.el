45419ba5996eeac1ab41fdca6a392f1a100a2644
Rajat Vig <rajat.vig@gmail.com>
2015-05-01 15:37:13 -0700

    elpa updates

--- elpa/racket-mode-20150420.1514/racket-mode-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "racket-mode" "20150420.1514" "Major mode for Racket language."
-  '((emacs "24.3")
-    (dash "2.10.0")
-    (faceup "0.0.2")
-    (s "1.9.0"))
-  :url "https://github.com/greghendershott/racket-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
