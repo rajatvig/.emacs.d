1edbae5577a3b1e1496fb3520848562220b82eec
Rajat Vig <rajat.vig@gmail.com>
2015-05-12 16:10:11 -0700

    elpa updates

--- elpa/avy-20150508.1418/avy-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "avy" "20150508.1418" "set-based completion"
-  '((emacs "24.1"))
-  :url "https://github.com/abo-abo/avy" :keywords
-  '("point" "location"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
